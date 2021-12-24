2de9b90964f2a562457a82f53593f48e800bc911
mayank18100 <mayank18100@iiitnr.edu.in>
2021-12-25 01:17:57 +0530

    model added

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, url_for, request
 from gtts import gTTS
+from extra_functions import extract_features, generate_desc
+from pickle import load
+from keras.models import load_model
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
 @app.route("/")
@@ -12,9 +15,13 @@ def results():
         #getting the image and saving it...
         f = request.files['file']
         f.save("static/img")
-
+        features = extract_features('static/img')
+        model = load_model('model_19.h5')
+        tokens = load(open('tokenizer.pkl','rb'))
+        max_length = 34
+        gen_caption = generate_desc(model, tokens, features, max_length)
         #generating the caption/description according to input image...
-        gen_caption = "A beautiful white colored house is sorrounded by lovely mountains."
+        # gen_caption = "A beautiful white colored house is sorrounded by lovely mountains."
 
         #converting text to speech and saving it...
         tts = gTTS(text= "The description for the input image is : " + gen_caption + "Thank You", lang='en', tld="com")
---
+++ extra_functions.py
@@ -0,0 +1,45 @@
+from numpy import argmax
+import PIL.Image
+from keras.applications.vgg16 import VGG16, preprocess_input
+from keras.preprocessing.image import load_img, img_to_array
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Model
+# from pickle import dump, load
+# from numpy import array
+# from keras.preprocessing.text import Tokenizer
+# from keras.utils import to_categorical
+# from keras.layers.merge import add
+# from keras.models import load_model
+# from keras.layers import Input, Dense, LSTM, Embedding, Dropout
+# from keras.callbacks import ModelCheckpoint
+def extract_features(filename):
+    model = VGG16()
+    model.layers.pop()
+    model = Model(inputs=model.inputs, outputs=model.layers[-1].output)
+    image = load_img(filename, target_size=(224, 224))
+    image = img_to_array(image)
+    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+    image = preprocess_input(image)
+    feature = model.predict(image, verbose=0)
+    return feature
+
+def word_for_id(integer, tokenizer):
+    for word, index in tokenizer.word_index.items():
+        if index == integer:
+            return word
+    return None
+
+def generate_desc(model, tokenizer, photo, max_length):
+    in_text = 'startseq'
+    for i in range(max_length):
+        sequence = tokenizer.texts_to_sequences([in_text])[0]
+        sequence = pad_sequences([sequence], maxlen=max_length)
+        yhat = model.predict([photo,sequence], verbose=0)
+        yhat = argmax(yhat)
+        word = word_for_id(yhat, tokenizer)
+        if word is None:
+            break
+        in_text += ' ' + word
+        if word == 'endseq':
+            break
+    return in_text
